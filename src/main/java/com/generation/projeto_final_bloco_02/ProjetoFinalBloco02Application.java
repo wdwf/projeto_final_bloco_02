3ed0eff4cc344666b1037fec2ffb3bbf8a0ca923
wdwf <w_dwf@hotmail.com>
2025-05-05 09:49:38 -0300

    :tada: Inicialização do projeto

---
+++ src/main/java/com/generation/projeto_final_bloco_02/ProjetoFinalBloco02Application.java
@@ -0,0 +1,13 @@
+package com.generation.projeto_final_bloco_02;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoFinalBloco02Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoFinalBloco02Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/generation/projeto_final_bloco_02/ProjetoFinalBloco02ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.generation.projeto_final_bloco_02;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoFinalBloco02ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
